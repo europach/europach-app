5d22da781c6c87101702f13108d445ac11183e7a
europach <48749610+europach@users.noreply.github.com>
2019-03-27 14:22:01 +0100

    Update EUEvents.js

--- src/events/EUEvents.js
+++ src/events/EUEvents.js
@@ -1487,7 +1487,7 @@ url: 'www.euro.who.int/__data/assets/pdf_file/0004/98437/E93312.pdf'
         content: ''
       },
     ],
-    startDate: '1949/01/01',
+    startDate: '2012/04/02',
     endDate: '',
     logics: [LOGICS.securitisation, LOGICS.publicHealth],
     linksWith: ['Documented_HIV_Outbreak_Among', 'UNGASS_Statement_by_Greek', 'Ongoing_Forced_HIV_Testing', 'Inter-Organisational_Coalition_with', 'Operation_Xenios_Zeus', ' Joint_Letter_to_UN'],
